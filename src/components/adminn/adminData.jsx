69982ca23c41a72050879328aa68707785bfbded
Jay Ann <jayannlgarcia@iskolarngbayan.pup.edu.ph>
2023-07-18 01:47:16 +0800

    Fixed Home Admin

--- src/components/adminn/adminData.jsx
+++ src/components/adminn/adminData.jsx
@@ -1,20 +1,30 @@
-
 export const AdminData = [
     {
-    adKey:1,
-    eventsCreated: <h4 style={{fontWeight: 'bold', fontFamily: 'Arial'}}>20</h4> 
+        adKey: 1,
+        eventsCreated: (
+            <div>
+                <h4 style={{ fontWeight: 'bold', fontFamily: 'Arial' }}>20</h4>
+                <p style={{ color: '#8c8c8c', fontFamily: 'Arial', fontSize: '20px' }}>Events Created</p>
+            </div>
+        )
     },
     {
-    adKey:2,
-    userRegistration: <h4 style={{fontWeight: 'bold', fontFamily: 'Arial'}}>560</h4>
+        adKey: 2,
+        userRegistration: (
+            <div>
+                <h4 style={{ fontWeight: 'bold', fontFamily: 'Arial' }}>560</h4>
+                <p style={{ color: '#8c8c8c', fontFamily: 'Arial', fontSize: '20px' }}>User Registrations</p>
+            </div>
+        )
     },
     {
-    adKey: 3,
-    ticketSales: <h4 style={{fontWeight: 'bold', fontFamily: 'Arial'}}>280</h4>
+        adKey: 3,
+        ticketSales: (
+            <div>
+                <h4 style={{ fontWeight: 'bold', fontFamily: 'Arial' }}>280</h4>
+                <p style={{ color: '#8c8c8c', fontFamily: 'Arial', fontSize: '20px' }}>Ticket Sales</p>
+            </div>
+        
+        )
     },
-    {
-    adKey: 4,
-    TotalEarnings: <h4 style={{fontWeight: 'bold', fontFamily: 'Arial'}}>10,000</h4>
-    }
-    
 ];
